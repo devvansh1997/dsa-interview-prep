ce884c58bf19a3c5c88c62461d50b485cfbb1d78
Devansh Sharma <dev@Devanshs-MacBook-Air.local>
2026-05-10 14:07:33 -0400

    some code adjustments and notes

--- phase-01-arrays-hashing/242_validAnagram.py
+++ phase-01-arrays-hashing/242_validAnagram.py
@@ -7,16 +7,10 @@ class Solution:
         # define dicts
         s_map, t_map = {}, {}
 
-        for idx, val in enumerate(s):
-            # s_map
-            if val not in s_map:
-                s_map[val] = 1
-            else: s_map[val] += 1
+        s_map, t_map = {}, {}
+        for c in s:
+            s_map[c] = s_map.get(c, 0) + 1
+        for c in t:
+            t_map[c] = t_map.get(c, 0) + 1
         
-        for idx, val in enumerate(t):
-            # t_map
-            if val not in t_map:
-                t_map[val] = 1
-            else: t_map[val] += 1
-        
-        return True if (s_map == t_map) else False
+        return s_map == t_map
--- phase-01-arrays-hashing/README.md
+++ phase-01-arrays-hashing/README.md
@@ -1,1 +1,3 @@
 # Hashing Mental Notes
+
+- use dict.get() for counts or keeping track of items in dict
